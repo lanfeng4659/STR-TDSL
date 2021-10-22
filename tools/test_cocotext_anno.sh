ce1bd63bba37de83c53437b5c2624c002b2c6c5c
lanfeng4659 <lanfeng4659@github.com>
2021-10-22 10:36:24 +0800

    update

--- tools/test_cocotext_anno.sh
+++
@@ -1,4 +0,0 @@
-# CUDA_VISIBLE_DEVICES=0 python tools/test_net.py --config-file "configs/retrival_ic13.yaml" --ckpt ./model_domain3_7078.pth
-CUDA_VISIBLE_DEVICES=2 python tools/test_net.py --config-file "configs/retrival_cocotext_anno.yaml" --ckpt ./model_rec_7052.pth
-# python tools/show_ic13.py
-# python evaluation/Pascal_VOC.py 
